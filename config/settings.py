6490d3b7e3f56ede99929cc119180523618f140c
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-01 13:17:00 +0300

    Media setting

--- config/settings.py
+++ config/settings.py
@@ -148,3 +148,6 @@ rest_framework = {
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /"staticFiles"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
