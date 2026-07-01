9f1b7cea70d3240e3b4fd26c111f8fef1f2fcbf9
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-01 13:19:54 +0300

    media urls seetings

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import static
 from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -26,3 +28,9 @@ urlpatterns = [
     path('api/schema', SpectacularAPIView.as_view()),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name="schema"),name="swagger_ui" )
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root = settings.MEDIA_ROOT
+    )
