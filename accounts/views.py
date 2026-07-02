c1e5c22ef4cf10a44d60a96af557e35dabb80b67
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-02 23:11:21 +0300

    Built RegisterView

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 
+from .serializers import RegisterSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Creator
+
+class RegisterView(ModelViewSet):
+    queryset = Creator.objects.all()
+    serializer_class = RegisterSerializer
