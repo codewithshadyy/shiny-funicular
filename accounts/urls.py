5618f9500018ee72c6531590906284e4feca886d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-02 23:21:52 +0300

    fixess

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import RegisterView
 router = DefaultRouter()
-router.register(r"register", RegisterView.as_view(), basename="register")
+router.register(r"register", RegisterView, basename="register")
 
 
 urlpatterns = [
