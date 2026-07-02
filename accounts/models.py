9e855d54cec39e9c49de9e5777305bcd18ebc13d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-02 21:57:50 +0300

    Creator mOdel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+class Creator(AbstractUser):
+    
+    ROLE_CHOICES = (
+        ("creator","cre"),
+        ("moderator", "mod")
+    )
+    role = models.CharField(ROLE_CHOICES=ROLE_CHOICES, default="creator")
+    handle = models.CharField(unique=True)
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    USERNAME_FIELD = "handle"
+    REQUIRED_FIELDS = ['username']
+    
+    
+    def __str__(self):
+        return self.handle
+    
+    
