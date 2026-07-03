e32e1ad05b31236ad9ffc48135e8c426d3762abb
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-03 19:03:54 +0300

    login endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 router = DefaultRouter()
 router.register(r"register", RegisterView, basename="register")
+router.register(r"login", TokenObtainPairView, basename="login")
 
 
 urlpatterns = [
