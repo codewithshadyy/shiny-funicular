0f17a0ff4a70aaf52aefdfb8e26a50a5fb07d06b
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-03 19:12:55 +0300

    fixings url patterns error

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 router = DefaultRouter()
 router.register(r"register", RegisterView, basename="register")
-router.register(r"login", TokenObtainPairView.as_view(), basename="login")
+# router.register(r"login", TokenObtainPairView.as_view(), basename="login")
 
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("login/", TokenObtainPairView.as_view())
 ]
