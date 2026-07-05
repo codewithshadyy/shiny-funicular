22c697a7b2dfea9ea2a2b07137496311d208b963
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-05 12:54:05 +0300

    Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class Post(models.Model):
+    
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
+    
+    
+    def __str__(self):
+        return self.content
