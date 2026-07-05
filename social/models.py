02ba19fcde4b17dba4edc1ca9ac7ecc4233f5b39
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-05 15:19:59 +0300

    Follow model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+class Follow(models.Model):
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["follower", "following"], name="unique_pair_follow"
+            ),
+            models.CheckConstraint(
+                condition=~models.Q(follower=models.F("following")),
+                name="cannot follow itself"
+            )
+    
+        ]
+        
+        indexes = [
+            models.Index(fields=["follower", "-created_at"]),
+            
+            models.Index(fields=["following", "-created_at"])
+        ]
+        
+    def __str__(self):
+        return f"{self.follower.handle} -> {self.following.handle}"    
