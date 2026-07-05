5b138b032be821a289ef363cfbd414cc35d493dd
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-05 14:49:47 +0300

    Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,18 +1,33 @@
 from django.db import models
 from django.conf import settings
+import uuid
 
 
 
 class Post(models.Model):
     
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ["-created_at"]
-    
-    
-    def __str__(self):
-        return self.content
+   POST_TYPES_CHOICES = (
+       ("text", "Text"),
+       ("image", "Image"),
+       ("video", "Video"),
+       ("mixed", "Mixed"),
+       
+   )
+   
+   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+   post_type = models.CharField(choices=POST_TYPES_CHOICES, default="text")
+   content = models.TextField(blank=True)
+   like_count = models.PositiveIntegerField(default=0)
+   comment_count = models.PositiveIntegerField(default=0)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+   
+   
+   class Meta:
+       ordering = ["-created_at"]
+       indexes = [
+           models.Index(fields=["author", "created_at"])
+       ]
+              
+   
