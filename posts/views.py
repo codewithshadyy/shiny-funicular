3cce194a1306c0561b932bb279688cbfcc692bb5
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-06 13:49:44 +0300

    CreatePOstWithMediaView

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+import uuid
+from django.conf import settings
+import boto3
+from botocore.client import Config
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from rest_framework.generics import ListAPIView
-from .serializers import MediaSerializer, PostSerializer
-from .models import Post
+from .serializers import MediaSerializer, PostSerializer , MediaUploadRequestSerializer
+from .models import Post, Media
 from social.models import Follow
 from rest_framework.pagination import CursorPagination
 
@@ -23,3 +31,60 @@ class FeedView(ListAPIView):
             .select_related("author")
             .prefetch_related("media_items")
         )
+        
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        config=Config(signature_version="s3v4"),
+    )        
+
+class CreatePostWithMediaView():
+    
+    def post(self,request):
+        content = request.data.get("content", "")
+        media_type = request.data.get("media_type") 
+        file_extension = request.data.get("file_extension", "")  
+        
+        post_type = "text"
+        if media_type:
+            post_type = media_type
+        
+        post = Post.objects.create(
+            author=request.user,
+            content=content,
+            post_type=post_type,
+        )
+        
+        if not media_type:
+             return Response(PostSerializer(post).data, status=status.HTTP_201_CREATED)
+         
+        storage_key = f"posts/{post.id}/{uuid.uuid4()}.{file_extension}"   
+        media = Media.objects.create(
+            post=post,
+            media_type=media_type,
+            storage_key=storage_key,
+            processing_status="pending",
+        )
+        
+        s3_client = get_s3_client()
+        presigned_url = s3_client.generate_presigned_url(
+            "put_object",
+            Params={
+                "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
+                "Key": storage_key,
+            },
+            ExpiresIn=300, 
+        )
+
+        return Response(
+            {
+                "post": PostSerializer(post).data,
+                "media_id": str(media.id),
+                "upload_url": presigned_url,
+                "storage_key": storage_key,
+            },
+            status=status.HTTP_201_CREATED,
+        )   
