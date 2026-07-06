cb02cf144dbce7408d75136fac5b0b3a71f006be
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-06 12:06:46 +0300

    Cretade FeedView with pagination func

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListAPIView
+from .serializers import MediaSerializer, PostSerializer
+from .models import Post
+from social.models import Follow
+from rest_framework.pagination import CursorPagination
+
+class FeedCursorPagination(CursorPagination):
+    page_size = 20
+    ordering = "-created_at"
+    cursor_query_param = "cursor"
+
+class FeedView(ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = FeedCursorPagination
+    
+    def get_queryset(self):
+        following_ids = Follow.objects.filter(follower=self.request.user).values_list("following_id", flat=True)
+        
+        return (
+            Post.objects.filter(author_id__in=following_ids)
+            .select_related("author")
+            .prefetch_related("media_items")
+        )
