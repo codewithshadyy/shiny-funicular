80a51a71a97b2144a0a928202698a1fe0338956d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-06 13:50:32 +0300

    posts/urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import FeedView
+from .views import FeedView, CreatePostWithMediaView
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
+    path("create/", CreatePostWithMediaView.as_view(), name="create-post"),
 ]
+
