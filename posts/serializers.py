8e8f45cfc00f18344345391dca1c2b11b15378af
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-06 11:56:48 +0300

    PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,4 +10,16 @@ class MediaSerializer(serializers.ModelSerializer):
             "duration_seconds", "thumbnail_key", "width", "height", "order",
         ]
 
+class PostSerializer(serializers.ModelSerializer):
+    author_handle = serializers.CharField(source="author.handle", read_only=True)
+    media_items = MediaSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Post
+        fields =  [
+            "id", "author_handle", "post_type", "content",
+            "like_count", "comment_count", "created_at", "media_items",
+        ]
+        read_only_fields = ["like_count", "comment_count", "created_at"]
+        
         
