43673c8e308c10fe30a30ebaceaf8eda8f171b9a
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-07 17:42:09 +0300

    urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FeedView, CreatePostWithMediaView, ConfirmMediaUploadView
+from .views import FeedView, CreatePostWithMediaView, ConfirmMediaUploadView, PushFeedView
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
+    path("feed/push/", PushFeedView.as_view(), name="feed-push"),
     path("create/", CreatePostWithMediaView.as_view(), name="create-post"),
     path("media/<uuid:media_id>/confirm/", ConfirmMediaUploadView.as_view(), name="confirm-media"),
 ]
