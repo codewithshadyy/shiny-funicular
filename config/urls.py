eb54c6dc429b05a9f5d96ff5433520f066ed986a
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-08 13:42:20 +0300

    main/urls

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
+import ossaudiodev
+import sys
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,7 +28,7 @@ from drf_spectacular.views import (
 )
 
 def whoami(request):
-    return JsonResponse({"served_by_port": request.get_host()})
+    return JsonResponse({"served_by_port": request.META.get("SERVER_PORT")})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
