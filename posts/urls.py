4e91eb201c48c3ccfdee09f2f4b9417b2470705c
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-08 23:49:42 +0300

    posts/urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import FeedView, CreatePostWithMediaView, ConfirmMediaUploadView, PushFeedView
+from .views import FeedView, CreatePostWithMediaView, ConfirmMediaUploadView, PushFeedView, ToggleLikeView, CommentListCreateView
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
     path("feed/push/", PushFeedView.as_view(), name="feed-push"),
     path("create/", CreatePostWithMediaView.as_view(), name="create-post"),
     path("media/<uuid:media_id>/confirm/", ConfirmMediaUploadView.as_view(), name="confirm-media"),
+    path("<uuid:post_id>/like/", ToggleLikeView.as_view(), name="toggle-like"),
+    path("<uuid:post_id>/comments/", CommentListCreateView.as_view(), name="post-comments")
+    
 ]
 
