ee24e8176df96a6e039b59089441b3eacf3c7910
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-07-09 09:38:32 +0300

    accounts/urls:logoutView configured

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, LogoutView
 router = DefaultRouter()
 router.register(r"register", RegisterView, basename="register")
 # router.register(r"login", TokenObtainPairView.as_view(), basename="login")
@@ -9,6 +9,7 @@ router.register(r"register", RegisterView, basename="register")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("login/", TokenObtainPairView.as_view()),
-    path("token/refresh/", TokenRefreshView.as_view())
+    path("token/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
